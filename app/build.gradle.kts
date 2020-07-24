348b411aa65b211d52b3a88214b01680949e5ed3
Brady Aiello <brady.aiello@gmail.com>
2020-07-24 09:41:18 -0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    kotlin("kapt")
+    kotlin("android.extensions")
+    kotlin("android")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdkVersion(30)
+    buildToolsVersion("30.0.1")
+
+    defaultConfig {
+        applicationId = "com.codingwithmitch.hiltexperiment"
+        minSdkVersion(23)
+        targetSdkVersion(30)
+        versionCode(1)
+        versionName("1.0")
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            minifyEnabled(false)
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    val kotlinVersion = rootProject.extra.get("kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+    implementation("androidx.core:core-ktx:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.1.0")
+    implementation("com.google.android.material:material:1.1.0")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+    testImplementation("junit:junit:4.13")
+    androidTestImplementation("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
+
+    // Hilt
+    val hiltVersion = "2.28-alpha"
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+}
