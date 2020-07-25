8be316b45b01c136f94cf1cbe82d2a29987b9df9
Brady Aiello <brady.aiello@gmail.com>
2020-07-24 21:32:10 -0700

    Lesson 5 Modules, @Binds, and @Provides https://codingwithmitch.com/courses/hilt-dependency-injection/modules-binds-and-provides/ Showed how to provide an interface and a 3rd party library with @Binds and @Provides. @Binds can't inject a 3rd party library because it doesn't have any annotation to tell it how to instantiate it. @Provides does both, so use @Provides unless we learn something about cases where @Binds would be better.

--- app/src/main/java/com/codingwithmitch/hiltexperiment/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/hiltexperiment/MainActivity.kt
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -37,7 +44,7 @@ class MyFragment: Fragment() {
  may not reference bindings with different scopes:
 */
 class SomeClass @Inject constructor(
-        //private val someInterface: SomeInterface,
+        private val someInterface: SomeInterface,
         private val gson: Gson
 ) {
     fun doAThing(): String = "Look I got a thing" //${someInterface.getAThing()}"
@@ -53,4 +60,33 @@ constructor(): SomeInterface {
 
 interface SomeInterface {
     fun getAThing(): String
+}
+
+/*@InstallIn(ActivityComponent::class)
+@Module
+abstract class MyModule {
+    @ActivityScoped
+    @Binds
+    abstract fun bindSomeDependency(
+            someImpl: SomeInterfaceImpl
+    ): SomeInterface
+
+    @ActivityScoped
+    @Binds
+    // This won't compile, because Dagger doesn't know how to instantiate Gson.
+    abstract fun bindGson(
+            gson: Gson
+    ): Gson
+}*/
+@InstallIn(ActivityComponent::class)
+@Module
+class MyModule {
+
+    @ActivityScoped
+    @Provides
+    fun provideSomeInterface(): SomeInterface = SomeInterfaceImpl()
+
+    @ActivityScoped
+    @Provides
+    fun provideGson(): Gson = Gson()
 }
