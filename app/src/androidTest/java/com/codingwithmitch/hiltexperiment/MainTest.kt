d9756e800d6d01f3f17949417817204d9604d0fe
Brady Aiello <brady.aiello@gmail.com>
2020-07-26 15:18:25 -0700

    Lesson 9 Hilt makes testing easier on Android Part 2 We replace a module in dependency graph for testing by simply using @UninstallModules on the current one, and installing a test module in its place.

--- app/src/androidTest/java/com/codingwithmitch/hiltexperiment/MainTest.kt
+++ app/src/androidTest/java/com/codingwithmitch/hiltexperiment/MainTest.kt
@@ -1,15 +1,29 @@
 package com.codingwithmitch.hiltexperiment
 
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import com.codingwithmitch.hiltexperiment.di.AppModule
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import org.hamcrest.CoreMatchers.containsString
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Inject
+import javax.inject.Singleton
 
+@UninstallModules(AppModule::class)
 @HiltAndroidTest
 class MainTest {
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var someString: String
 
     @Before
     fun init() {
@@ -18,6 +32,14 @@ class MainTest {
 
     @Test
     fun someTest() {
+        assertThat(someString, containsString("TESTING"))
+    }
 
+    @Module
+    @InstallIn(ApplicationComponent::class)
+    object TestAppModule {
+        @Singleton
+        @Provides
+        fun provideSomeString(): String = "It's some TESTING string!"
     }
 }
